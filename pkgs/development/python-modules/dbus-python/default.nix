52bb4232ffda8a806b83eb17dda1314f4a2c0f2d
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-12-04 03:13:01 +0100

    python311Packages.dbus-python: 1.2.18 -> 1.3.2
    python311Packages.dbus-python: 1.2.18 -> 1.3.2
    
    Co-Authored-By: adisbladis <adisbladis@gmail.com>

---
+++ pkgs/development/python-modules/dbus-python/default.nix
@@ -0,0 +1,87 @@
+{ lib
+, fetchPypi
+, buildPythonPackage
+, isPyPy
+
+# build-system
+, meson
+, meson-python
+, ninja
+, patchelf
+, pkg-config
+, setuptools
+
+# native dependencies
+, dbus
+, dbus-glib
+
+# tests
+, pygobject3
+}:
+
+buildPythonPackage rec {
+  pname = "dbus-python";
+  version = "1.3.2";
+  pyproject = true;
+
+  disabled = isPyPy;
+
+  outputs = [
+    "out"
+    "dev"
+  ];
+
+  src = fetchPypi {
+    inherit pname version;
+    hash = "sha256-rWeBkwhhi1BpU3viN/jmjKHH/Mle5KEh/mhFsUGCSPg=";
+  };
+
+  postPatch = ''
+    # we provide patchelf natively, not through the python package
+    sed -i '/patchelf/d' pyproject.toml
+
+    # dont run autotols configure phase
+    rm configure.ac configure
+
+    patchShebangs test/*.sh
+  '';
+
+  nativeBuildInputs = [
+    meson
+    meson-python
+    ninja
+    patchelf
+    pkg-config
+    setuptools
+  ];
+
+  buildInputs = [
+    dbus
+    dbus-glib
+  ];
+
+  pypaBuildFlags = [
+    # Don't discard meson build directory, still needed for tests!
+    "-Cbuild-dir=_meson-build"
+  ];
+
+  nativeCheckInputs = [
+    dbus.out
+  ];
+
+  checkPhase = ''
+    runHook preCheck
+
+    meson test -C _meson-build --no-rebuild --print-errorlogs
+
+    runHook postCheck
+  '';
+
+  meta = with lib; {
+    description = "Python DBus bindings";
+    homepage = "https://gitlab.freedesktop.org/dbus/dbus-python";
+    license = licenses.mit;
+    platforms = dbus.meta.platforms;
+    maintainers = with maintainers; [ ];
+  };
+}
